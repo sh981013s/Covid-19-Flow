9a7a3ee682e01a4fee815b86a3a77b9bb9fb56a1
SeungHwan-Lee <sh981013s@gmail.com>
2021-10-15 18:21:33 +0900

    Initial Commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+
+const Header = () => {
+	return (
+		<header className='header'>
+			<h1>COVID-19 Flow</h1>
+			<select>
+				<option value=''>국내</option>
+				<option value=''>세계</option>
+			</select>
+		</header>
+	)
+}
+
+export default Header
